731d2e45c1bc7d92525330c9127c5a3ec54ec38b
Scott Humphries <git@sscotth.io>
2016-10-04 14:26:18 -0500

    Initial boilerplate

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require('express')
+const { Server } = require('http')
+const mongoose = require('mongoose')
+const socketio = require('socket.io')
+
+const app = express()
+const server = Server(app)
+const io = socketio(server)
+
+const PORT = process.env.PORT || 3000
+const MONGODB_URL =  process.env.MONGODB_URL || 'mongodb://localhost:27017/tickietackietoe'
+
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index'))
+
+mongoose.connect(MONGODB_URL, () => {
+  server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
+})
