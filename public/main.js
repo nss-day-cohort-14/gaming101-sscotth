72cda7b10770bec969721d91e1fac0519ee3408f
Scott Humphries <git@sscotth.io>
2016-10-05 01:57:41 -0500

    done

--- public/main.js
+++ public/main.js
@@ -2,108 +2,77 @@
 
 const socket = io()
 
-socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
-socket.on('disconnect', () => console.log('Socket disconnected'))
-
 const board = document.querySelector('.board')
 const status = document.querySelector('.status')
 
-const boardState = [
-  ['','',''],
-  ['','',''],
-  ['','',''],
-]
+const updateDisplay = (game) => {
+  renderStatus(getCurrentStatus(game))
+  renderBoard(game)
+}
 
-let nextPlayer = 'X'
+const getCurrentStatus = (game) => {
+  if (game.result && game.result === 'Tie') {
+    return 'Tie game'
+  }
 
-const drawBoard = b => {
+  if (game.result && game.result === 'Disconnect') {
+    return 'Player disconnected'
+  }
+
+  if (game.result) {
+    return `${game.result} Wins!`
+  }
+
+  if (!game.player1 || !game.player2) {
+    return 'Waiting for additional player'
+  }
+
+  if (game.nextTurn === `/#${socket.id}`) {
+    return 'Place your move'
+  }
+
+  return `Waiting for other player to move`
+}
+
+const renderStatus = s => status.innerText = s
+
+const renderBoard = game => {
+  // Needs more security
   board.innerHTML = `
     <table>
       <tr>
-        <td>${b[0][0]}</td>
-        <td>${b[0][1]}</td>
-        <td>${b[0][2]}</td>
+        <td>${game.board[0][0]}</td>
+        <td>${game.board[0][1]}</td>
+        <td>${game.board[0][2]}</td>
       </tr>
       <tr>
-        <td>${b[1][0]}</td>
-        <td>${b[1][1]}</td>
-        <td>${b[1][2]}</td>
+        <td>${game.board[1][0]}</td>
+        <td>${game.board[1][1]}</td>
+        <td>${game.board[1][2]}</td>
       </tr>
       <tr>
-        <td>${b[2][0]}</td>
-        <td>${b[2][1]}</td>
-        <td>${b[2][2]}</td>
+        <td>${game.board[2][0]}</td>
+        <td>${game.board[2][1]}</td>
+        <td>${game.board[2][2]}</td>
       </tr>
     </table>
   `
-
-  status.innerText = `${nextPlayer}'s Turn`
 }
-
-const winner = b => {
-  // Rows
-  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
-    return b[0][0]
-  }
-
-  if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
-    return b[1][0]
-  }
-
-  if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
-    return b[2][0]
-  }
-
-  // Cols
-  if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
-    return b[0][0]
-  }
-
-  if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
-    return b[0][1]
-  }
-
-  if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
-    return b[0][2]
-  }
-
-  // Diags
-  if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
-    return b[0][0]
-  }
-
-  if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
-    return b[0][2]
-  }
-
-  // Tie or In-Progress
-  else {
-    return null
-  }
-}
-
-drawBoard(boardState)
 
 board.addEventListener('click', evt => {
+  if (!evt.target.matches('td')) {
+    return
+  }
+
   const col = evt.target.cellIndex
   const row = evt.target.closest('tr').rowIndex
 
-  if (boardState[row][col]) {
-    return console.log('Cannot move there')
-  }
-
-  if (winner(boardState)) {
-    return console.log('Game is over!')
-  }
-
-  boardState[row][col] = nextPlayer
-  drawBoard(boardState)
-  console.log('Current game state:', board)
-
-  if (winner(boardState)) {
-    return status.innerText = `${nextPlayer} WON!`
-  }
-
-  nextPlayer = nextPlayer === 'X' ? 'O' : 'X'
-  status.innerText = `${nextPlayer}'s Turn`
+  socket.emit('make move', { col, row })
 })
+
+socket.on('connect', () => console.log(`Socket connected: ${socket.id}`))
+socket.on('disconnect', () => console.log('Socket disconnected'))
+socket.on('waiting for player', updateDisplay)
+socket.on('game start', updateDisplay)
+socket.on('game end', updateDisplay)
+socket.on('move made', updateDisplay)
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index'))
 
+mongoose.Promise = Promise
 mongoose.connect(MONGODB_URL, () => {
   server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`))
 })
@@ -27,10 +28,181 @@ const Game = mongoose.model('game', {
     [String, String, String],
     [String, String, String],
     [String, String, String],
-  ]
+  ],
+  player1: String,
+  player2: String,
+  nextTurn: String,
+  result: String,
 })
 
 io.on('connect', socket => {
-  console.log(`Socket connected: ${socket.id}`)
-  socket.on('disconnect', () => console.log(`Socket disconnected: ${socket.id}`))
+  matchNewSocketToGame(socket)
+  socket.on('make move', () => handleMove(move, socket))
+  socket.on('disconnect', () => handleDisconnect(socket))
 })
+
+// ----- MATCHMAKING LOGIC -----
+
+function matchNewSocketToGame (socket) {
+  Game.findOne().sort('-_id').then(game => {
+    // if there are no games in the database or the last one already has two players, start a new game
+    if (!game || hasTwoPlayers(game)) {
+      return Game.create({
+        board: [['','',''],['','',''],['','','']],
+        [`player${randomPlayer1or2()}`]: socket.id,
+      })
+    }
+
+    // otherwise add as other player
+    if (game.player1) {
+
+      game.player2 = socket.id
+    } else {
+      game.player1 = socket.id
+    }
+
+    game.nextTurn = game.player1
+    return game.save()
+  })
+  .then(game => {
+    // emit status
+    socket.gameId = game._id
+    socket.join(`/${game._id}`)
+
+    if (hasTwoPlayers(game)) {
+      return io.to(`/${game._id}`).emit('game start', game)
+    }
+
+    return socket.emit('waiting for player', game)
+  })
+}
+
+function randomPlayer1or2 () {
+  return Math.round(Math.random()) + 1
+}
+
+// ----- MOVE HANDLING LOGIC -----
+
+function handleMove (move, socket) {
+  Game.findById(socket.gameId).then(game => {
+    if (isMoveAllowed(move, socket, game)) {
+      makeMove(move, socket, game)
+        .then(g => io.to(`/${socket.gameId}`).emit('move made', g))
+    }
+  })
+}
+
+function isMoveAllowed (move, socket, game) {
+  return hasTwoPlayers(game)
+    && isMyMove(socket, game)
+    && isFinished(game)
+    && isMoveAvailable(move, game)
+}
+
+function isMyMove (socket, game) {
+  return socket.id === game.nextTurn
+}
+
+function isFinished (game) {
+  return !game.result
+}
+
+function isMoveAvailable (move, game) {
+  return !game.board[move.row][move.col];
+}
+
+function makeMove (move, socket, game) {
+  game.board[move.row][move.col] = mapPlayerToToken(socket, game)
+  game.markModified('board') // trigger mongoose change detection
+
+  const winner = getWinner(game)
+
+  if (winner) {
+    game.result = winner
+  } else if (!movesRemaining(game)) {
+    game.result = 'Tie'
+  } else {
+    game.nextTurn = getNextTurn(game)
+  }
+
+  return game.save()
+}
+
+
+function mapPlayerToToken (socket, game) {
+  return socket.id === game.player1 ? 'X' : 'O'
+}
+
+function getWinner (game) {
+  const b = game.board
+  // Rows
+  if (b[0][0] && b[0][0] === b[0][1] && b[0][1] === b[0][2]) {
+    return b[0][0]
+  }
+
+  if (b[1][0] && b[1][0] === b[1][1] && b[1][1] === b[1][2]) {
+    return b[1][0]
+  }
+
+  if (b[2][0] && b[2][0] === b[2][1] && b[2][1] === b[2][2]) {
+    return b[2][0]
+  }
+
+  // Cols
+  if (b[0][0] && b[0][0] === b[1][0] && b[1][0] === b[2][0]) {
+    return b[0][0]
+  }
+
+  if (b[0][1] && b[0][1] === b[1][1] && b[1][1] === b[2][1]) {
+    return b[0][1]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][2] && b[1][2] === b[2][2]) {
+    return b[0][2]
+  }
+
+  // Diags
+  if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+    return b[0][0]
+  }
+
+  if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+    return b[0][2]
+  }
+
+  // Tie or In-Progress
+  return null
+}
+
+function movesRemaining (game) {
+  const POSSIBLE_MOVES = 9
+  return POSSIBLE_MOVES - flatten(game.board).join('').length
+}
+
+function getNextTurn (game) {
+  return game.nextTurn === game.player1 ? game.player2 : game.player1
+}
+
+
+// ----- EARLY DISCONNECT LOGIC -----
+
+function handleDisconnect (socket, game) {
+  Game.findById(socket.gameId).then(game => {
+    if (!game.result) {
+      game.result = 'Disconnect'
+      game.save().then(g => io.to(`/${socket.gameId}`).emit('game end', g))
+    }
+  })
+}
+
+// ----- USED BY MULTIPLE -----
+
+function hasTwoPlayers (game) {
+  return game.player1 && game.player2
+}
+
+// ----- UTILITY FUNCTIONS -----
+
+function flatten (array) {
+  return array.reduce((a,b) => a.concat(b), [])
+}
